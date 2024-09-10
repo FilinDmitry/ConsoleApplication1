e10419c1c1e75358cee8fda29b83e5d1afb8a3eb
FilinDmitry <148043353+FilinDmitry@users.noreply.github.com>
2024-09-10 11:00:52 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int chislo = 20;
+	char s1[] = "fdsf";
+	double d1 = 25;
+	float f1 = 25.6;
+	char ch1 = 3;
+	int a1, b1, b3;
+	int a2 = 25;
+	char a3 = 'h';
+	string str = "Hello";
+	char b2 = 'B';
+	int a5, a4, c3 = 18;
+	char s2 = 'hi';
+	char c[] = "lets go";
+	cout << "Hello" << endl;
+	return 0;
+}
