ba3254b166cc27478a0336376bce02a146029ccc
FilinDmitry <148043353+FilinDmitry@users.noreply.github.com>
2024-09-24 10:18:24 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include<windows.h>
 using namespace std;
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int i, a, b;
     float c;
     cout << "Введите действие 1 - сложение, 2 - умножение, 3 - вычитание, 4 - целочисленное деление, 5 - остаток от деления";
