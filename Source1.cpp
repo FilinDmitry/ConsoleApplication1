63c07b0e02d1b08bb681c0731a54638b54effb8a
FilinDmitry <148043353+FilinDmitry@users.noreply.github.com>
2024-09-24 08:31:03 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
-#include <windjws.h>
 using namespace std;
 
 int main()
 {
     int i, a, b;
-    cout << "Введите действие 1 - деление, 2 - умножение, 3 - вычитание, 4 - деление, 5 - целочисленное деление";
+    float c;
+    cout << "Введите действие 1 - сложение, 2 - умножение, 3 - вычитание, 4 - целочисленное деление, 5 - остаток от деления";
     cin >> i;
-    cout << "Введите a"
+    cout << "Введите a" << endl;
     cin >> a;
-    cout << "Введите b"
+    cout << "Введите b" << endl;
     cin >> b;
     if (i==1)
     {
-        
+        c = a + b;
+        cout << "a + b = " << c << endl;
+    }
+    if (i==2)
+    {
+        c = a * b;
+        cout << "a * b = " << c << endl;
+    }
+    if (i==3)
+    {
+        c = a - b;
+        cout << "a - b = " << c << endl;
+    }
+    if (i==4)
+    {
+        c = a / b;
+        cout << "a / b = " << c << endl;
+    }
+    if (i==5)
+    {
+        c = a % b;
+        cout << "a % b = " << c << endl;
     }
 }
